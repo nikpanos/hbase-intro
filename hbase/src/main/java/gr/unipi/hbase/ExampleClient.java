7190d14003de012b19b7a14d6672a88a8566e369
Panagiotis Nikitopoulos <nikp@unipi.gr>
2019-01-14 17:39:07 +0200

    Migrated to version 1.2.9

--- hbase/src/main/java/gr/unipi/hbase/ExampleClient.java
+++ hbase/src/main/java/gr/unipi/hbase/ExampleClient.java
@@ -1,38 +1,35 @@
 package gr.unipi.hbase;
 import java.io.IOException;
+
+import com.google.protobuf.ServiceException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.Get;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
-import org.apache.hadoop.hbase.client.HTable;
-import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.client.ResultScanner;
-import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.util.Bytes;
 
 public class ExampleClient {
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException, ServiceException {
     Configuration config = HBaseConfiguration.create();
     // Create table
-    HBaseAdmin admin = new HBaseAdmin(config);
+    HBaseAdmin.checkHBaseAvailable(config);
+    Connection connection = ConnectionFactory.createConnection(config);
+    Admin admin = connection.getAdmin();
     try {
       TableName tableName = TableName.valueOf("test");
-      HTableDescriptor htd = new HTableDescriptor(tableName);
-      HColumnDescriptor hcd = new HColumnDescriptor("data");
-      htd.addFamily(hcd);
-      admin.createTable(htd);
+      HTableDescriptor desc = new HTableDescriptor(tableName);
+      desc.addFamily(new HColumnDescriptor("data"));
+      admin.createTable(desc);
       HTableDescriptor[] tables = admin.listTables();
       if (tables.length != 1 &&
           Bytes.equals(tableName.getName(), tables[0].getTableName().getName())) {
-        throw new IOException("Failed create of table");
+        throw new IOException("Failed to create table");
       }
       // Run some operations -- three puts, a get, and a scan -- against the table.
-      HTable table = new HTable(config, tableName);
+      Table table = connection.getTable(tableName);
       try {
         for (int i = 1; i <= 3; i++) {
           byte[] row = Bytes.toBytes("row" + i);
@@ -40,7 +37,7 @@ public class ExampleClient {
           byte[] columnFamily = Bytes.toBytes("data");
           byte[] qualifier = Bytes.toBytes(String.valueOf(i));
           byte[] value = Bytes.toBytes("value" + i);
-          put.add(columnFamily, qualifier, value);
+          put.addColumn(columnFamily, qualifier, value);
           table.put(put);
         }
         Get get = new Get(Bytes.toBytes("row1"));
